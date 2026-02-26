3a94726254ac9b7b3a9ac04d973b6f2a1a5754d9
eric <esgrazio@loyola.edu>
2026-02-25 20:23:08 -0500

    fix missing variable

--- docker/training/train.py
+++ docker/training/train.py
@@ -2,18 +2,18 @@ from sklearn.svm import SVC
 from sklearn import datasets
 import joblib
 
-#Load the Iris dataset
+# Load the Iris dataset
 iris = datasets.load_iris()
 
-#Create an SVM classifier
+# Create an SVM classifier
 clf = SVC()
 
-#Train the model using the iris dataset
+# Train the model using the iris dataset
 model = clf.fit(iris.data, iris.target_names[iris.target])
 
-# TODO: Save the trained model to the shared volume (make sure to use the correct path)
-joblib.dump(model, ...)
+# Save the trained model to the shared volume (make sure to use the correct path)
+fout = 'iris_model.pkl'
+joblib.dump(model, f'/app/models/{fout}')
 
-print("Model training complete and saved as iris_model.pkl")
-
+print(f"Model training complete and saved as {fout}")
 
